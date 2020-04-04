c1be400a6a9b5eb9ffb7f8c511c6a1ff942b3f4e
Jared De Blander <jared0x90@gmail.com>
2020-04-04 04:25:34 -0400

    Starting to parse values

--- app/src/main/java/com/example/myapplication/SecondFragment.kt
+++ app/src/main/java/com/example/myapplication/SecondFragment.kt
@@ -5,9 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
-import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_second.*
 
 /**
@@ -27,7 +25,29 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         button_looks_good.setOnClickListener {
-            Toast.makeText(context, "Please review your information", Toast.LENGTH_LONG).show()
+
+            val weight_empty = edit_weight.text.toString().isEmpty()
+            val height_empty = edit_height.text.toString().isEmpty()
+
+            if(weight_empty || height_empty) {
+                Toast.makeText(context, "Please respond to all questions above", Toast.LENGTH_LONG).show()
+            } else {
+
+                val weight = edit_weight.text.toString().toDouble()
+                val height = edit_height.text.toString().toDouble()
+                val age = 37
+
+                if(height > 24 && height < 108 && weight > 50 && weight < 3000) {
+                    val bmi = 703.0717 * (weight / (height*height))
+                    val maleBmr = (66 + (6.2 * weight) + (12.7 * height) - (6.76 * age))
+                    val femaleBmr = (655.1 + 4.35 * weight + 4.7 * height - 4.7 * age)
+                    Toast.makeText(context, ("BMI = %.2f\nmBMR = %.2f\n" +
+                            "fBMR = %.2f").format(bmi, maleBmr, femaleBmr), Toast.LENGTH_LONG).show()
+                }
+            }
+
+
+
         }
 
 //
