6797c22667c03f4e6bad293b7418b601cdafab33
Jared De Blander <jared0x90@gmail.com>
2020-05-03 16:22:23 -0400

    todo note updates

--- app/src/main/java/com/example/myapplication/Todo.kt
+++ app/src/main/java/com/example/myapplication/Todo.kt
@@ -4,7 +4,8 @@ package com.example.myapplication
  * - finish onboard activity
  * - attach image to favorite
  * - take picture with camera and save picture to filesystem
- * = reimpliment reset button
+ * - rewrite FirstFragment's layout xml as a linear vertical layout
+ * - round values displayed to 1 decimal place around the app
  */
 
 object Todo {
