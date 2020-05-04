cc3ad7434a19fba6f4613ba525cfa622893e5c86
Jared De Blander <jared0x90@gmail.com>
2020-05-04 10:54:32 -0400

    todo upates

--- app/src/main/java/com/example/myapplication/Todo.kt
+++ app/src/main/java/com/example/myapplication/Todo.kt
@@ -1,11 +1,15 @@
 package com.example.myapplication
 
 /***
+ * todo possilby finished
  * - finish onboard activity
+ *
+ * todo
  * - attach image to favorite
  * - take picture with camera and save picture to filesystem
  * - rewrite FirstFragment's layout xml as a linear vertical layout
  * - round values displayed to 1 decimal place around the app
+ * - age vs birthdate
  */
 
 object Todo {
