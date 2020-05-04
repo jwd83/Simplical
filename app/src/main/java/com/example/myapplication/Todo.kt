7520219bea413e46bb285cd7b5b68bd698c4d8ba
Jared De Blander <jared0x90@gmail.com>
2020-05-04 11:10:48 -0400

    Manifest update

--- app/src/main/java/com/example/myapplication/Todo.kt
+++ app/src/main/java/com/example/myapplication/Todo.kt
@@ -1,17 +1,18 @@
 package com.example.myapplication
 
 /***
- * todo possilby finished
- * - finish onboard activity
- *
- * todo
- * - attach image to favorite
- * - take picture with camera and save picture to filesystem
- * - rewrite FirstFragment's layout xml as a linear vertical layout
- * - round values displayed to 1 decimal place around the app
- * - age vs birthdate
- * - setup discord integration
- */
+* todo possilby finished
+* - finish onboard activity
+* - setup discord integration
+*
+*
+* todo
+* - attach image to favorite
+* - take picture with camera and save picture to filesystem
+* - rewrite FirstFragment's layout xml as a linear vertical layout
+* - round values displayed to 1 decimal place around the app
+* - age vs birthdate
+*/
 
 object Todo {
     fun todo(): String {
