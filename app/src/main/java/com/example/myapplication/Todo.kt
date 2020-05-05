bfcb1b9dce60bf7398afe9180e88801e3aededfe
Jared De Blander <jared0x90@gmail.com>
2020-05-05 13:54:38 -0400

    Updating todo list

--- app/src/main/java/com/example/myapplication/Todo.kt
+++ app/src/main/java/com/example/myapplication/Todo.kt
@@ -6,19 +6,18 @@ package com.example.myapplication
  * setup discord integration
  * rewrite FirstFragment's layout xml as a linear vertical layout
  * add debug button to clear oboard complete flag
+ * age vs birthdate storage
  *
  * todo
  * attach image to favorite
  * take picture with camera and save picture to filesystem
  * round values displayed to 1 decimal place around the app
- * age vs birthdate
+ * age calculations re-added
  * merge settings and second fragment into a single reusable fragment
-
 */
 
 object Todo {
     fun todo(): String {
         return "todo"
     }
-
 }
