c1308d4f27993bb7a62ff1c6b83390fe5f3fbb4f
Jared De Blander <jared0x90@gmail.com>
2020-05-18 22:12:07 -0400

    Planning of pacing display in main menu

--- app/src/main/java/me/jwd/simplical/Todo.kt
+++ app/src/main/java/me/jwd/simplical/Todo.kt
@@ -1,20 +1,9 @@
 package me.jwd.simplical
 
 /***
- * todo possibly finished
- * finish onboard activity
- * setup discord integration with github
- * rewrite FirstFragment's layout xml as a linear vertical layout
- * add debug button to clear onboard complete flag
- * age vs birth date storage
- * fix maintain option
- * show more data on review your day
- * make day in review more clear
- * removed CALORIES_CONSUMED_DATE & CALORIES_CONSUMED keys
- * add calories consumed per day to home screen
- * periodic redraw on home fragment
- *
+
  * todo
+ * add a weekly pace at daily pace goal or current cal and show lesser of two. i.e. 2.0 lb/week or 1.8 when dipping below your goal
  * look futher into scheduling reminder system/calendar functionality
  * attach image to favorite
  * take picture with camera and save picture to filesystem
@@ -27,6 +16,21 @@ package me.jwd.simplical
  * create icon for google play
  * disclaimer
  * make calorie range toggle to show only loss/gain or both tdee and loss/gain
+*
+ * todo possibly finished
+ * reorganized this list
+ * finish onboard activity
+ * setup discord integration with github
+ * rewrite FirstFragment's layout xml as a linear vertical layout
+ * add debug button to clear onboard complete flag
+ * age vs birth date storage
+ * fix maintain option
+ * show more data on review your day
+ * make day in review more clear
+ * removed CALORIES_CONSUMED_DATE & CALORIES_CONSUMED keys
+ * add calories consumed per day to home screen
+ * periodic redraw on home fragment
+ *
  */
 
 object Todo {
